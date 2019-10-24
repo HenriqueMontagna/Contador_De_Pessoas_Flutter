b5bbc929d0cc75e55732f0820befbaab285a9d44
Henrique La Montagna <henriquelamontagna@gmail.com>
2019-10-24 17:07:15 -0300

    Subindo o proj inicial

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: "Contador de Pessoas",
+        home: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text("+1",
+                        style: TextStyle(color: Colors.white, fontSize: 40)),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text("-1",
+                        style: TextStyle(color: Colors.white, fontSize: 40)),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Pode Entrar!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontStyle: FontStyle.italic,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
